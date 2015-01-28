5c62faa7cab73e5ae82a164f9a58ff4f63ceeeda
roy <roy_radio@126.com>
2015-01-28 10:20:15 +0800

    building static site generator

---
+++ chapter03/sitebuilder/urls.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from django.conf.urls import url
+
+from .views import page
+
+urlpatterns = (
+    url(r'^(?P<slug>[\w./-]+)/$', page, name='page'),
+    url(r'^$', page, name='homepage'),
+)
